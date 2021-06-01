540995372d680a3b521c67d58ce913637d3ffa34
Chatchai Sirithipvanich <chatchai.sirithipvanith@gmail.com>
2021-06-01 11:20:59 +0700

    add language detection function, change recording methods to records if shifted is pressed while typing characters, add flatten function to convert new records to simple records also add method in RecordSession to return simple records instead of new records

--- kbana/capture/keyboard_capture.py
+++ kbana/capture/keyboard_capture.py
@@ -1,8 +1,24 @@
 import keyboard
+import json
 import pickle
 import os
 import datetime
 import copy
+import ctypes
+
+
+def get_keyboard_language():
+    # only language not variant layout
+    user32 = ctypes.WinDLL('user32', use_last_error=True)
+    curr_window = user32.GetForegroundWindow()
+    thread_id = user32.GetWindowThreadProcessId(curr_window, 0)
+    klid = user32.GetKeyboardLayout(thread_id)
+    lid = klid & (2 ** 16 - 1)
+    lid_hex = f"{lid:#0{6}x}"
+    root = os.path.dirname(__file__)
+    with open(root + 'misc/win-language-id.json', 'r') as f:
+        win_layout = json.load(f)
+    return win_layout[lid_hex]
 
 
 def load_records(filename):
@@ -11,9 +27,22 @@ def load_records(filename):
     return records
 
 
+def flatten_records(records):
+    records = records
+    combined = records[False]
+    shifted = records[True]
+    for k in shifted:
+        if k in combined:
+            combined[k] += shifted[k]
+        else:
+            combined[k] = shifted[k]
+    return combined
+
+
 class RecordingSession:
     def __init__(self, filename=None, directory=None):
         self.time_stamp = datetime.datetime.now().strftime("%y_%m_%d-%H_%M_%S")
+        self._shift_state = False
         if filename:
             # if filename is provided ignore directory
             self._filename = filename
@@ -35,20 +64,38 @@ class RecordingSession:
             records = load_records(self._filename)
 
         else:
-            records = {}
+            records = {True: {}, False: {}}
         self._records = records
 
     def record_key(self, key_event):
         record_book = self._records
-        k = key_event.scan_code
-        if k in record_book:
-            record_book[k] += 1
+        if key_event.event_type == 'up':
+            k = key_event.scan_code
+            if k in record_book[self._shift_state]:
+                record_book[self._shift_state][k] += 1
+            else:
+                record_book[self._shift_state][k] = 1
+
+    def shift_toggle(self):
+        if self._shift_state:
+            self._shift_state = False
         else:
-            record_book[k] = 1
+            self._shift_state = True
 
     def save_recording(self):
         with open(self._filename, "wb") as f:
             pickle.dump(self._records, f)
+
+    def records_key_only(self):
+        records = self.records
+        combined = records[False]
+        shifted = records[True]
+        for k in shifted:
+            if k in combined:
+                combined[k] += shifted[k]
+            else:
+                combined[k] = shifted[k]
+        return combined
 
     # use property decorator so that user cannot directly modified records
     @property
