e3b7e3626a201de8cfdac0bf0b66c1f45ee0ad17
Chatchai Sirithipvanich <chatchai.sirithipvanith@gmail.com>
2021-06-20 16:04:17 +0700

    modified function default behavior

--- kbana/wraper.py
+++ kbana/wraper.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
-from .analysis import visualize_key_stroke_freq, visualize_finger_load, simulate_records
+from .analysis import visualize_key_stroke, visualize_finger_load, simulate_records
 
-def quick_plot(text, layout):
+def quick_plot(text, layout, numeric='freq'):
     """
     text: string
     layout: string
@@ -13,6 +13,8 @@ def quick_plot(text, layout):
     """
     records = simulate_records(text, layout)
     fig, ax = plt.subplots(2)
-    visualize_key_stroke_freq(records, log_scale=False, axis_handle=ax[0])
-    visualize_finger_load(records, axis_handle=ax[1])
+    visualize_key_stroke(records, log_scale=False, axis_handle=ax[0],
+                         exclude_key_list=[29, 91, 56, 57, 14, 42, 54, 15, 58, 28, 'n/a'],
+                         numeric=numeric)
+    visualize_finger_load(records, axis_handle=ax[1], numeric=numeric)
     return 0
