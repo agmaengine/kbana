b74022a8c9339370d3fb2884b323b47962ac05d5
Chatchai Sirithipvanich <chatchai.sirithipvanith@gmail.com>
2021-06-25 15:11:57 +0700

    quick plot return recording for gui to be able to exprot them

--- src/kbana/wrapper.py
+++ src/kbana/wrapper.py
@@ -13,14 +13,21 @@ def quick_plot(text, layout=None, numeric='freq'):
     please specify layout so that input text can be mapped to scan codes associated to the layout
     """
     if type(text) == dict:
-        records = text
+        recording = text
     elif type(text) == str:
-        records = simulate_recording(text, layout)
+        recording = simulate_recording(text, layout)
     else:
         raise ValueError('text must be stringType or dictionaryType')
-    fig, ax = plt.subplots(2)
-    visualize_key_stroke(records, log_scale=False, axis_handle=ax[0],
+    fig = plt.figure()
+    ax1 = plt.subplot(211)
+    ax2 = plt.subplot(223)
+    ax3 = plt.subplot(224)
+    visualize_key_stroke(recording, log_scale=False, axis_handle=ax1,
                          exclude_key_list=[29, 91, 56, 57, 14, 42, 54, 15, 58, 28, 'n/a'],
                          numeric=numeric)
-    visualize_finger_load(records, axis_handle=ax[1], numeric=numeric, exclude_shift=False)
-    return fig
+    ax1.set_title("key stroke on keyboard")
+    visualize_finger_load(recording, axis_handle=ax2, numeric=numeric, exclude_shift=False)
+    ax2.set_title("finger load include shift key press")
+    visualize_finger_load(recording, axis_handle=ax3, numeric=numeric, exclude_shift=True)
+    ax3.set_title("finger load exclude shift key press")
+    return fig, recording
